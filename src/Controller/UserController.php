a2f4e83d1361c5edc0efd9850d8a1a5f2fd2d35a
BartlomiejMus <bartlomiejmus@gmail.com>
2022-10-19 13:32:44 +0200

    init commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+/**
+ * @Route("/api/user", name="api_user_")
+ */
+
+class UserController extends AbstractFOSRestController
+{
+    private Security $security;
+
+    public function __construct
+    (
+        Security $security
+    )
+    {
+        $this->security = $security;
+    }
+
+    /**
+     * @Rest\Get("", name="get_by_id")
+     *
+     */
+    public function getUserAction()
+    {
+        return $this->security->getUser();
+    }
+}
