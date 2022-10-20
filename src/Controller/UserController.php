7cbf6b09bbda94c8e5d9de6164ab84dae1471b15
LigockiD <ymstaff2@gmail.com>
2022-10-20 15:37:42 +0200

    zzz

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\Post;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Validator\Constraints\Json;
 
 /**
  * @Route("/api/user", name="api_user_")
@@ -31,4 +38,17 @@ class UserController extends AbstractFOSRestController
     {
         return $this->security->getUser();
     }
+
+    /**
+     * @Rest\Post("", name="create")
+     * @Rest\View()
+     * @ParamConverter("user", converter="fos_rest.request_body")
+     */
+    public function postUserAction(User $user) : JsonResponse
+    {
+
+        dd($user);
+        return new JsonResponse(null,Response::HTTP_CREATED);
+    }
+
 }
