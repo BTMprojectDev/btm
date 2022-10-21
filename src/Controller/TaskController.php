7c36c97435a2cd5e0884863022549f2985898e28
BartlomiejMus <bartlomiejmus@gmail.com>
2022-10-21 14:58:28 +0200

    Task show function completed

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,15 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Task;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\TaskRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 /**
  * @Route("/api/task", name="api_task")
@@ -19,29 +14,32 @@ use Symfony\Component\Serializer\Serializer;
 
 class TaskController extends AbstractFOSRestController
 {
-    /**
-     * @Rest\Get("/all", name="get_all_task")
-     * @View(serializerGroups={"task"})
-     */
-    public function allTasks(ManagerRegistry $doctrine):JsonResponse
+    private TaskRepository $taskRepository;
+
+    public function __construct
+    (
+        TaskRepository $taskRepository,
+    )
     {
-        $tasks =$doctrine->getRepository(Task::class)->findAll();
+        $this->taskRepository = $taskRepository;
+    }
 
-        if (!$tasks) {
-            throw $this->createNotFoundException(
-                'No product found for id '
-            );
-        }
+    /**
+     * @Rest\Get("/{task}", name="get_by_id")
+     * @Rest\View(serializerGroups={"task"})
+     */
+    public function show(Task $task)
+    {
+        return $task;
+    }
 
-        //dd($tasks);
-
-//        $encoders = [new JsonEncoder()];
-//        $normalizers = [new ObjectNormalizer()];
-//
-//        $serializer = new Serializer($normalizers, $encoders);
-//        $content = $serializer->serialize($tasks, 'json');
-
-        return new JsonResponse($tasks);
+    /**
+     * @Rest\Get("", name="get_all_task")
+     * @Rest\View(serializerGroups={"task"})
+     */
+    public function all()
+    {
+        return $this->taskRepository->findAll();
     }
 }
 
