6e3730756308c3cac985abbf7e7e00630644d0e6
BartlomiejMus <bartlomiejmus@gmail.com>
2022-10-21 11:23:15 +0200

    TaskController1

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,26 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\Task;
-use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
- * @Route("/api/task", name="api_task_")
+ * @Route("/api/task", name="api_task")
  */
 
 class TaskController extends AbstractFOSRestController
 {
     /**
-     * @Rest\Get("/{id}", name="get_by_id")
-     * @Rest\View(serializerGroups={"task"})
-     *
+     * @Rest\Get("/all", name="get_all_task")
+     * @View(serializerGroups={"task"})
      */
-    public function getTaskAction(Task $task)
+    public function allTasks(ManagerRegistry $doctrine):JsonResponse
     {
-        return $task;
+        $tasks =$doctrine->getRepository(Task::class)->findAll();
+
+        if (!$tasks) {
+            throw $this->createNotFoundException(
+                'No product found for id '
+            );
+        }
+
+        //dd($tasks);
+
+//        $encoders = [new JsonEncoder()];
+//        $normalizers = [new ObjectNormalizer()];
+//
+//        $serializer = new Serializer($normalizers, $encoders);
+//        $content = $serializer->serialize($tasks, 'json');
+
+        return new JsonResponse($tasks);
     }
 }
 
