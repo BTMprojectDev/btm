fdca9d454c12941c8eda4f6e6338324cc99306b3
LigockiD <ymstaff2@gmail.com>
2022-10-25 10:57:42 +0200

    user controller complete?

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
+
+use App\Form\UserType;
 use App\Repository\UserRepository;
-use DateTimeImmutable;
 use Doctrine\Persistence\ManagerRegistry;
+use Exception;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcherInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,19 +28,22 @@ class UserController extends AbstractFOSRestController
     private ManagerRegistry $managerRegistry;
     private UserRepository $userRepository;
     private UserPasswordHasherInterface $userPasswordHasher;
+    private ParamFetcherInterface $paramFetcher;
 
     public function __construct
     (
         Security $security,
         ManagerRegistry $managerRegistry,
         UserRepository $userRepository,
-        UserPasswordHasherInterface $userPasswordHasher
+        UserPasswordHasherInterface $userPasswordHasher,
+        ParamFetcherInterface $paramFetcher
     )
     {
         $this->security = $security;
         $this->managerRegistry = $managerRegistry;
         $this->userRepository = $userRepository;
         $this->userPasswordHasher = $userPasswordHasher;
+        $this->paramFetcher = $paramFetcher;
     }
 
     /**
@@ -60,13 +65,16 @@ class UserController extends AbstractFOSRestController
     }
 
     /**
-     * @Rest\Delete("/{id}", name="delete_user_by_id")
+     * @Rest\Delete("", name="delete_user_by_id")
+     * @QueryParam(name="user_id", requirements="\d+", description="user_id")
      * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
      */
-    public function delete(int $id)
+    public function delete()
     {
+        $id = $this->paramFetcher->get("user_id");
         $user = $this->userRepository->findOneBy(['id' => $id]);
         $this->managerRegistry->getManager()->remove($user);
+        $this->managerRegistry->getManager()->flush();
         return new JsonResponse(
             null,
             Response::HTTP_NO_CONTENT
@@ -75,83 +83,57 @@ class UserController extends AbstractFOSRestController
 
     /**
      * @Rest\Put("/{id}", name="put_by_user_id")
+     * @QueryParam(name="user_id", requirements="\d+", description="user_id")
      * @Rest\View(statusCode=Response::HTTP_CREATED)
      */
-    public function put(int $id, Request $request)
+    public function put(Request $request)
     {
-        $oldUser = $this->userRepository->find($id);
+        $id = $this->paramFetcher->get("user_id");
+        $existingUser = $this->userRepository->find($id);
         $newUser = $request->request->all();
-
-        $oldUser->setEmail($newUser['email']);
+        $form = $this->createForm(UserType::class, $existingUser);
 
         $plainPassword = $newUser['password'];
         $hashedPassword = $this->userPasswordHasher->hashPassword(
-            $oldUser,
+            $existingUser,
             $plainPassword
         );
-        $oldUser->setPassword($hashedPassword);
+        $newUser["password"] = $hashedPassword;
 
-        $oldUser->setLat($newUser['lat']);
-
-        $oldUser->setLng($newUser['lng']);
-
-        $oldUser->setPhotoUrl($newUser['photoUrl']);
-
-        $oldUser->setFirstName($newUser['firstName']);
-
-        $oldUser->setLastName($newUser['lastName']);
-
-        $date = new DateTimeImmutable($newUser['dateOfBirth']);
-        $oldUser->setDateOfBirth($date);
+        $form->submit($newUser, false);
+        if(false === $form->isValid()){
+            return new JsonResponse(
+                null,
+                Response::HTTP_BAD_REQUEST
+            );
+        }
 
         $this->managerRegistry->getManager()->flush();
 
-        return $oldUser;
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
 
     }
 
     /**
-     * @Rest\Patch("/{id}", name="patch_by_user_id")
+     * @Rest\Patch("", name="patch_by_user_id")
      * @Rest\View(statusCode=Response::HTTP_CREATED)
-     * @throws \Exception
+     * @QueryParam(name="user_id", requirements="\d+", description="user_id")
+     * @throws Exception
      */
-    public function patch(int $id, Request $request)
+    public function patch(Request $request)
     {
-        $oldUser = $this->userRepository->find($id);
+        $id = $this->paramFetcher->get("user_id");
+        $existingUser = $this->userRepository->find($id);
         $newUser = $request->request->all();
-        if($newUser['email']){
-            $oldUser->setEmail($newUser['email']);
-        }
-        if($newUser['password']){
-            $plainPassword = $newUser['password'];
-            $hashedPassword = $this->userPasswordHasher->hashPassword(
-                $oldUser,
-                $plainPassword
+        $form = $this->createForm(UserType::class, $existingUser);
+        $form->submit($newUser, false);
+        if(false === $form->isValid()){
+            return new JsonResponse(
+                null,
+                Response::HTTP_BAD_REQUEST
             );
-            $oldUser->setPassword($hashedPassword);
-        }
-        if($newUser['lat']){
-            $oldUser->setLat($newUser['lat']);
-        }
-        if($newUser['lng']){
-            $oldUser->setLng($newUser['lng']);
-        }
-        if($newUser['photoUrl']){
-            $oldUser->setPhotoUrl($newUser['photoUrl']);
-        }
-        if($newUser['firstName']){
-            $oldUser->setFirstName($newUser['firstName']);
-        }
-        if($newUser['lastName']){
-            $oldUser->setLastName($newUser['lastName']);
-        }
-        if($newUser['dateOfBirth']){
-            $date = new DateTimeImmutable($newUser['dateOfBirth']);
-            $oldUser->setDateOfBirth($date);
         }
         $this->managerRegistry->getManager()->flush();
-
-        return $oldUser;
-
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
